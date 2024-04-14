46943d297b46eb87978216ae56da7a18adb1b60e
Gabriel Mendes <gabri.mevial@gmail.com>
2024-04-14 15:31:48 -0300

    requisito 4 finalizado

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,3 +1,4 @@
+import json
 from typing import Dict, Type
 from abc import ABC, abstractmethod
 from inventory_report.product import Product
@@ -12,8 +13,25 @@ class Importer(ABC):
         pass
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self):
+        with open(self.path, 'r') as file:
+            data = json.load(file)
+
+        products = []
+        for item in data:
+            product = Product(
+                id=item['id'],
+                product_name=item['product_name'],
+                company_name=item['company_name'],
+                manufacturing_date=item['manufacturing_date'],
+                expiration_date=item['expiration_date'],
+                serial_number=item['serial_number'],
+                storage_instructions=item['storage_instructions']
+            )
+            products.append(product)
+
+        return products
 
 
 class CsvImporter:
